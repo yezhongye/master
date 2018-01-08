10955b828982256c1020903977a57b22e3e8db67
zhoujingxuan <zhoujingxuan@qq.com>
2018-01-08 16:40:36 +0800

    添加mybatis查询数据库类

--- springboot/com.ye.springboot.dubbo/3.3-com.ye.dubbo.user.dao/src/main/java/com/ye/dubbo/user/dao/mapper/TbBaseEmployeeMapper.java
+++ springboot/com.ye.springboot.dubbo/3.3-com.ye.dubbo.user.dao/src/main/java/com/ye/dubbo/user/dao/mapper/TbBaseEmployeeMapper.java
@@ -1,6 +1,10 @@
 package com.ye.dubbo.user.dao.mapper;
 
+import com.ye.db.employee.entity.pages.PageWhere;
 import com.ye.dubbo.user.dao.entity.TbBaseEmployee;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface TbBaseEmployeeMapper {
     int insert(TbBaseEmployee record);
@@ -12,4 +16,8 @@ public interface TbBaseEmployeeMapper {
     int updateByPrimaryKeySelective(TbBaseEmployee record);
 
     int updateByPrimaryKey(TbBaseEmployee record);
+
+    /*--==================================================华丽分割线==================================================--*/
+    List<TbBaseEmployee> selectSelective(@Param("record")TbBaseEmployee record,@Param("pageWhere")PageWhere pageWhere);
+
 }
